2bf811c4d3dddf1482e745422f805b8735d9fefd
Raul <Raul@LAPTOP-4SN6V5T6>
2017-11-14 23:12:20 -0400

    Creación del constructor y algunas clases.

--- ProyectoFinal/src/logico/Tricom.java
+++ ProyectoFinal/src/logico/Tricom.java
@@ -1,6 +1,59 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Tricom {
+	private ArrayList<Empleados> misEmpleados;
+	private ArrayList<Plan> misPlanes;
+	private static Tricom tricom;
 	
+	private Tricom()
+	{
+		super();
+		this.misEmpleados = new ArrayList<>();
+		this.misPlanes = new ArrayList<>();
+	}
+	
+	public static Tricom getInstance()
+	{
+		if(tricom == null)
+		   tricom = new Tricom();
+		
+		return tricom;
+	}
+	
+	private Plan buscarPlan(String codPlan)
+	{
+		boolean encontrado = false;
+		int i = 0;
+		Plan plan = null;
+		
+		while(i < misPlanes.size() && encontrado == false)
+		{
+			if(misPlanes.get(i).getCodPlan().equalsIgnoreCase(codPlan))
+			{
+				plan = misPlanes.get(i);
+			}
+			i++;
+		}
+		return plan;
+	}
+	
+	
+	public ArrayList<Empleados> getMisEmpleados() {
+		return misEmpleados;
+	}
+
+	public void setMisEmpleados(ArrayList<Empleados> misEmpleados) {
+		this.misEmpleados = misEmpleados;
+	}
+
+	public ArrayList<Plan> getMisPlanes() {
+		return misPlanes;
+	}
+
+	public void setMisPlanes(ArrayList<Plan> misPlanes) {
+		this.misPlanes = misPlanes;
+	}
 	
 }
