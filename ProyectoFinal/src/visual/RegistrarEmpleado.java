4850a9a44794a9843f73566f419d5c8acb417785
Raul <Raul@LAPTOP-4SN6V5T6>
2017-11-26 22:46:10 -0400

    Imagenes

--- ProyectoFinal/src/visual/RegistrarEmpleado.java
+++ ProyectoFinal/src/visual/RegistrarEmpleado.java
@@ -233,7 +233,7 @@ public class RegistrarEmpleado extends JDialog {
 								
 	
 								
-								Empleados vend = new Empleados(nombreUser.getText(),apellidoUser.getText(),cedulaUser.getText(),direccionUser.getText(),telefonoUser.getText(),txtnotam.getText(), spnsal.getValue(),contraUser.getText());
+								//Empleados vend = new Empleados(nombreUser.getText(),apellidoUser.getText(),cedulaUser.getText(),direccionUser.getText(),telefonoUser.getText(),txtnotam.getText(), spnsal.getValue(),contraUser.getText());
 								
 								String m = correoUser.getText();
 								
