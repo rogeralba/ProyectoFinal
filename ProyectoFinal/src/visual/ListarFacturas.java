4c8dda66c7431f277c3cd252f878e3c9dc16f398
g8-bi <g8-bi@LAPTOP-O9P0KUKK>
2017-12-04 01:10:48 -0400

    Edits en las clases ListarFacturas y PagarFactura (errores)

--- ProyectoFinal/src/visual/ListarFacturas.java
+++ ProyectoFinal/src/visual/ListarFacturas.java
@@ -6,12 +6,22 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class ListarFacturas extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
+	private static JTable table;
+	private static DefaultTableModel tableModel;
+	static Object[] fila; 
 	/**
 	 * Launch the application.
 	 */
@@ -29,25 +39,36 @@ public class ListarFacturas extends JDialog {
 	 * Create the dialog.
 	 */
 	public ListarFacturas() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 560, 430);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			JButton btnSalir = new JButton("Salir");
+			btnSalir.setBounds(437, 324, 97, 44);
+			btnSalir.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					dispose();
+				}
+			});
+			contentPanel.setLayout(null);
+			contentPanel.add(btnSalir);
+		}
+		{
+			JLabel label = new JLabel("\u00A9 2017 Tricom. Todos los derechos reservados.");
+			label.setBounds(10, 351, 291, 16);
+			contentPanel.add(label);
+		}
+		{
+			JPanel panel = new JPanel();
+			panel.setBackground(Color.WHITE);
+			panel.setBounds(10, 11, 524, 287);
+			contentPanel.add(panel);
+			panel.setLayout(new BorderLayout(0, 0));
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JScrollPane scrollPane = new JScrollPane();
+				panel.add(scrollPane, BorderLayout.CENTER);
 			}
 		}
 	}
--- ProyectoFinal/src/visual/PagarFactura.java
+++ ProyectoFinal/src/visual/PagarFactura.java
@@ -13,10 +13,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
 
 import logico.Cliente;
+import logico.Controladora;
+import logico.Tricom;
 
 import java.awt.Font;
 import java.text.ParseException;
@@ -25,11 +28,14 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PagarFactura extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField correoCliente;
 	private static JTable table;
 	private static DefaultTableModel tableModel;
 	static Object[] fila;
@@ -69,12 +75,16 @@ public class PagarFactura extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
+		String[] columnheaders = {"ID", "Cedula", "Nombre", "Plan", "Precio"};
+		tableModel = new DefaultTableModel();
+		tableModel.setColumnIdentifiers(columnheaders);
+		
 		JLabel label = new JLabel("\u00A9 2017 Tricom. Todos los derechos reservados.");
 		label.setBounds(10, 464, 291, 16);
 		contentPanel.add(label);
 		try {
 			ced = new MaskFormatter("###-#######-#");
-			tele = new MaskFormatter("(809)###-####");
+			tele = new MaskFormatter("(###)-###-####");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -122,7 +132,24 @@ public class PagarFactura extends JDialog {
 		lblNombre.setBounds(331, 30, 71, 25);
 		panel.add(lblNombre);
 		
-		JComboBox cbxNombre = new JComboBox();
+		cbxNombre = new JComboBox();
+		cbxNombre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(cbxNombre.getSelectedIndex() != 0){
+					nombre = cbxNombre.getSelectedItem().toString();
+					client = Tricom.getInstance().buscarCliente(nombre);
+					
+					if(client != null)
+					{
+						txtCedula.setText(client.getCodCli());
+						txtCorreo.setText(client.getEmail());
+						txtTelefono.setText(client.getTelefono());
+						
+						cargarFacturas();
+					}
+				}
+			}
+		});
 		cbxNombre.setSelectedIndex(0);
 		cbxNombre.setBounds(412, 29, 182, 27);
 		panel.add(cbxNombre);
@@ -166,11 +193,20 @@ public class PagarFactura extends JDialog {
 		panel_2.add(label_4);
 		
 		JButton btnPagar = new JButton("Pagar");
+		btnPagar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnPagar.setEnabled(false);
 		btnPagar.setBounds(517, 436, 97, 44);
 		contentPanel.add(btnPagar);
 		
 		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		btnSalir.setBounds(405, 436, 97, 44);
 		contentPanel.add(btnSalir);
 		
@@ -181,5 +217,55 @@ public class PagarFactura extends JDialog {
 		
 		table_1 = new JTable();
 		scrollPane.setColumnHeaderView(table_1);
+	}
+
+	protected void cargarFacturas() {
+		// TODO Auto-generated method stub
+		tableModel.setRowCount(0);
+		DefaultTableCellRenderer tcr = new DefaultTableCellRenderer();
+		tcr.setHorizontalAlignment(SwingConstants.CENTER);
+		table.getColumnModel().getColumn(0).setCellRenderer(tcr);
+		table.getColumnModel().getColumn(1).setCellRenderer(tcr);
+		table.getColumnModel().getColumn(2).setCellRenderer(tcr);
+		table.getColumnModel().getColumn(3).setCellRenderer(tcr);
+		table.getColumnModel().getColumn(4).setCellRenderer(tcr);
+		
+		fila = new Object[tableModel.getColumnCount()];
+		
+		for(int i=0; i < Tricom.getInstance().getListafactura().size();i++)
+		{
+			if(Tricom.getInstance().getListafactura().get(i).getCliente().getCedula().equalsIgnoreCase(client.getCedula()))
+			{
+				fila[0] = Tricom.getInstance().getListafactura().get(i).getIdFactura();
+				fila[1] = Tricom.getInstance().getListafactura().get(i).getCliente().getCedula();
+				fila[2] = Tricom.getInstance().getListafactura().get(i).getCliente().getNombre();
+				
+				if(Tricom.getInstance().getListafactura().get(i).getCliente().getPlanes().getTipo().toString().equalsIgnoreCase("Cable"))
+				{
+					fila[3] = "Cable";
+				}
+				else if(Tricom.getInstance().getListafactura().get(i).getCliente().getPlanes().getTipo().toString().equalsIgnoreCase("Internet"))
+				{
+					fila[3] = "Internet";
+				}
+				else if(Tricom.getInstance().getListafactura().get(i).getCliente().getPlanes().getTipo().toString().equalsIgnoreCase("Telefonia"))
+				{
+					fila[3] = "Telefono";
+				}
+				else if(Tricom.getInstance().getListafactura().get(i).getCliente().getPlanes().getTipo().toString().equalsIgnoreCase("todo"))
+				{
+					fila[3] = "Todo";
+				}
+				else
+				{
+					fila[3] = "Combinado";
+				}
+				
+				fila[4] = Tricom.getInstance().getListafactura().get(i).getCliente().getPlanes().getPrecioNeto();
+				
+				tableModel.addRow(fila);
+			}
+			
+		}
 	}
 }
